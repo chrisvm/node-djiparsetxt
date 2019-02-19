5ccf9c8a747b592c820b996c212f87fd4d130496
cvelez <christian.velez55@gmail.com>
2019-02-19 15:18:21 -0400

    Fixed error in scrambling algo.

--- src/services/ScrambleTableService.ts
+++ src/services/ScrambleTableService.ts
@@ -4,7 +4,6 @@ import { get_scramble_table } from "./ScrambleTable";
 import { IRecord } from "./FileInfoService";
 
 export class ScrambleTableService extends BaseService {
-
 	private _scramble_table: number[][] = [];
 
 	public get scramble_table() {
@@ -14,35 +13,31 @@ export class ScrambleTableService extends BaseService {
 		return this._scramble_table;
 	}
 
-	public unscramble_record(record: IRecord): IRecord 
-  {
+	public unscramble_record(record: IRecord): IRecord {
 		let data = this.unscramble_buffer(record.data, record.type);
 		if (data == null) data = record.data;
-    return {
-      type: record.type,
-      length: record.length,
-      data: data
-    };
-  }
+		return {
+			type: record.type,
+			length: record.length,
+			data: data
+		};
+	}
 
 	private unscramble_buffer(buf: Buffer, type: RecordTypes): Buffer | null {
 		const first_byte = buf.readUInt8(0);
-		const scramble_key = first_byte | ((type - 1) << 8);
+		const scramble_key = ((type - 1) << 8) | first_byte;
 		const scramble_bytes = this.scramble_table[scramble_key];
 
 		if (scramble_bytes == undefined) {
 			return null;
 		}
 
-		const unscrambled_buf = Buffer.alloc(buf.length);
-
-		// write first byte which was already unscrambled
-		unscrambled_buf.writeUInt8(first_byte, 0);
+		const unscrambled_buf = Buffer.alloc(buf.length - 1);
 
 		for (let write_offset = 1; write_offset < buf.length; write_offset++) {
-			const scramble_entry = scramble_bytes[write_offset - (1 % 8)];
-			const val =	buf.readUInt8(write_offset) ^ scramble_entry;
-			unscrambled_buf.writeUInt8(val, write_offset);
+			const scramble_entry = scramble_bytes[(write_offset - 1) % 8];
+			const val = buf.readUInt8(write_offset) ^ scramble_entry;
+			unscrambled_buf.writeUInt8(val, write_offset - 1);
 		}
 
 		return unscrambled_buf;
