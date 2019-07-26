20d02aa564e7a4c368596bd6a6e8fa4e7fba2ca0
cvelez <christian.velez55@gmail.com>
2019-07-26 10:37:30 -0400

    Added spark automated tests.

--- tests/file_version.test.ts
+++ tests/file_version.test.ts
@@ -1,52 +1,39 @@
-import path from "path";
-import fs from "fs";
 import { expect } from "chai";
+import fs from "fs";
+import path from "path";
 import { parse_file } from "../src/node-djiparsetxt";
 
-describe('Mavic 2 Log Test', () => {
-	const filesDir = path.join(__dirname, '../assets/flight_logs/mavic2');
-	const fileList = fs.readdirSync(filesDir);
-	
+function createTestFromDir(filePath: string) {
+	const fileList = fs.readdirSync(filePath);
+
 	for (let fileName of fileList) {
 		it(`should parse file ${fileName}`, () => {
-			fileName = path.join(filesDir, fileName);
+			fileName = path.join(filePath, fileName);
 			const buffer = fs.readFileSync(fileName);
 			const rows = parse_file(buffer);
-			
+
 			expect(rows).to.be.an("array");
 			expect(rows.length).to.be.greaterThan(0);
 		});
 	}
+}
+
+describe("Mavic 2 Log Tests", () => {
+	const filesDir = path.join(__dirname, "../assets/flight_logs/mavic2");
+	createTestFromDir(filesDir);
 });
 
-describe('Phantom 3 Log Test', () => {
-	const filesDir = path.join(__dirname, '../assets/flight_logs/phantom3');
-	const fileList = fs.readdirSync(filesDir);
-	
-	for (let fileName of fileList) {
-		it(`should parse file ${fileName}`, () => {
-			fileName = path.join(filesDir, fileName);
-			const buffer = fs.readFileSync(fileName);
-			const rows = parse_file(buffer);
-			
-			expect(rows).to.be.an("array");
-			expect(rows.length).to.be.greaterThan(0);
-		});
-	}
+describe("Phantom 3 Log Tests", () => {
+	const filesDir = path.join(__dirname, "../assets/flight_logs/phantom3");
+	createTestFromDir(filesDir);
 });
 
-describe('Phantom 4 Log Test', () => {
-	const filesDir = path.join(__dirname, '../assets/flight_logs/phantom4');
-	const fileList = fs.readdirSync(filesDir);
-	
-	for (let fileName of fileList) {
-		it(`should parse file ${fileName}`, () => {
-			fileName = path.join(filesDir, fileName);
-			const buffer = fs.readFileSync(fileName);
-			const rows = parse_file(buffer);
-			
-			expect(rows).to.be.an("array");
-			expect(rows.length).to.be.greaterThan(0);
-		});
-	}
+describe("Phantom 4 Log Tests", () => {
+	const filesDir = path.join(__dirname, "../assets/flight_logs/phantom4");
+	createTestFromDir(filesDir);
+});
+
+describe("Spark Log Tests", () => {
+	const filesDir = path.join(__dirname, "../assets/flight_logs/spark");
+	createTestFromDir(filesDir);
 });
