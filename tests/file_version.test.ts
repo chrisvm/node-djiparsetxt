8354f492f5e652ceead3eec84e8cc818e69e8275
cvelez <christian.velez55@gmail.com>
2020-05-29 02:42:46 -0400

    Added test catching the gps [0, 0] issue.

--- tests/file_version.test.ts
+++ tests/file_version.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { get_header, parse_file } from "../src/node-djiparsetxt";
+import { parse_file, IRowObject } from "../src/node-djiparsetxt";
 
 const junk = (item: string) => !(/(^|\/)\.[^\/\.]/g).test(item);
 
@@ -24,20 +24,60 @@ describe("Spark Log Tests", () => {
 	createTestFromDir(filesDir);
 });
 
+function filterFromKeys(row: IRowObject, keys: string[]): IRowObject {
+	const filtered: IRowObject = { filterCount: 0 };
+
+	if (keys.length == 0) return filtered;
+
+	for (let key of keys) {
+		if (key in row) {
+			filtered[key] = row[key];
+			filtered.filterCount += 1;
+		}
+	}
+
+	return filtered;
+}
+
 function createTestFromDir(filePath: string) {
 	const fileList = fs.readdirSync(filePath).filter(junk);
 
 	for (let fileName of fileList) {
-		it(`should parse file '${fileName}'`, () => {
-			fileName = path.join(filePath, fileName);
-			const buffer = fs.readFileSync(fileName);
+		describe(fileName, () => {
+			// parse file for this tests
+			const completeFileName = path.join(filePath, fileName);
+			const buffer = fs.readFileSync(completeFileName);
 			const rows = parse_file(buffer);
 
-			expect(Array.isArray(rows)).toBe(true);
-			expect(rows.length).toBeGreaterThan(0);
-			for (const row of rows) {
-				expect(row).toHaveProperty("OSD");
-			}
+			it(`should parse file '${fileName}'`, () => {
+				expect(Array.isArray(rows)).toBe(true);
+				expect(rows.length).toBeGreaterThan(0);
+				for (const row of rows) {
+					expect(row).toHaveProperty("OSD");
+				}
+			});
+
+			it('should remove the intial garbage created by the APP_GPS data (remove [0, 0] intial coord)', () => {
+				for (let row of rows) {
+					const key = 'APP_GPS';
+					if (key in row) {
+						const gps = row[key];
+						expect(Math.abs(gps.latitude)).toBeGreaterThan(0.0);
+						expect(Math.abs(gps.longitude)).toBeGreaterThan(0.0);
+					}
+				}
+			});
+
+			it('should not have values for gps that are exactly 0 in OSD record',  () => {
+				for (let row of rows) {
+					const key = 'OSD';
+					if (key in row) {
+						const gps = row[key];
+						expect(Math.abs(gps.latitude)).toBeGreaterThan(0.0);
+						expect(Math.abs(gps.longitude)).toBeGreaterThan(0.0);
+					}
+				}
+			});
 		});
 	}
 }
